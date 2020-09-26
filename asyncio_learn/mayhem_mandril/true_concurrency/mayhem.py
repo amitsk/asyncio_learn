30ae1639a5bcd758313e059fc84fde5fffb1506d
Amit Kumar <amitsk@users.noreply.github.com>
2020-09-26 14:42:10 -0700

    True concurrency walktrhough completed

--- asyncio_learn/mayhem_mandril/true_concurrency/mayhem.py
+++ asyncio_learn/mayhem_mandril/true_concurrency/mayhem.py
@@ -1,9 +1,36 @@
+#!/usr/bin/env python3.7
+# Copyright (c) 2018-2019 Lynn Root
+"""
+Adding concurrency to the `consume` coroutine function.
+
+Notice! This requires:
+ - attrs==19.1.0
+
+To run:
+
+    $ python part-1/mayhem_4.py
+
+Follow along: https://roguelynn.com/words/asyncio-true-concurrency/
+"""
+
 import asyncio
-import attr
-import string
+import logging
 import random
-from loguru import logger
+import string
 import uuid
+import functools
+import attr
+
+
+# NB: Using f-strings with log messages may not be ideal since no matter
+# what the log level is set at, f-strings will always be evaluated
+# whereas the old form ("foo %s" % "bar") is lazily-evaluated.
+# But I just love f-strings.
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 
 @attr.s
@@ -11,13 +38,23 @@ class PubSubMessage:
     instance_name = attr.ib()
     message_id = attr.ib(repr=False)
     hostname = attr.ib(repr=False, init=False)
+    restarted = attr.ib(repr=False, default=False)
+    saved = attr.ib(repr=False, default=False)
+    acked = attr.ib(repr=False, default=False)
+    extended_cnt = attr.ib(repr=False, default=0)
 
     def __attrs_post_init__(self):
         self.hostname = f"{self.instance_name}.example.net"
 
 
-async def publish(queue, publisher_id):
+async def publish(queue):
+    """Simulates an external publisher of messages.
+
+    Args:
+        queue (asyncio.Queue): Queue to publish messages to.
+    """
     choices = string.ascii_lowercase + string.digits
+
     while True:
         msg_id = str(uuid.uuid4())
         host_id = "".join(random.choices(choices, k=4))
@@ -25,44 +62,75 @@ async def publish(queue, publisher_id):
         msg = PubSubMessage(message_id=msg_id, instance_name=instance_name)
         # publish an item
         asyncio.create_task(queue.put(msg))
-        logger.info(f"[{publisher_id}] Published message {msg}")
+        logging.info(f"Published message {msg}")
         # simulate randomness of publishing messages
         await asyncio.sleep(random.random())
 
 
+async def save(msg):
+    # unhelpful simulation of i/o work
+    await asyncio.sleep(random.random())
+    msg.saved = True
+    logging.info(f"Saved {msg} into database")
+
+
+async def restart_host(msg):
+    # unhelpful simulation of i/o work
+    await asyncio.sleep(random.random())
+    msg.restart = True
+    logging.info(f"Restarted {msg.hostname}")
+
+
+# <--snip-->
+
+
+async def cleanup(msg, event):
+    # this will block the rest of the coro until `event.set` is called
+    await event.wait()
+    # unhelpful simulation of i/o work
+    await asyncio.sleep(random.random())
+    msg.acked = True
+    logging.info(f"Done. Acked {msg}")
+
+
+async def extend(msg, event):
+    while not event.is_set():
+        msg.extended_cnt += 1
+        logging.info(f"Extended deadline by 3 seconds for {msg}")
+        # want to sleep for less than the deadline amount
+        await asyncio.sleep(2)
+
+
+async def handle_message(msg):
+    event = asyncio.Event()
+
+    asyncio.create_task(extend(msg, event))
+    asyncio.create_task(cleanup(msg, event))
+
+    await asyncio.gather(save(msg), restart_host(msg))
+    event.set()
+
+
 async def consume(queue):
     while True:
-        # wait for an item from the publisher
         msg = await queue.get()
-
-        # the publisher emits None to indicate that it is done
-        if msg is None:
-            break
-
-        # process the msg
-        logger.info(f"Consumed {msg}")
-        # simulate i/o operation using sleep
-        await asyncio.sleep(random.random())
+        logging.info(f"Consumed {msg}")
+        asyncio.create_task(handle_message(msg))
 
 
 def main():
     queue = asyncio.Queue()
     loop = asyncio.get_event_loop()
 
-    coros = [publish(queue, i) for i in range(1, 4)]
-
     try:
-        [loop.create_task(coro) for coro in coros]
+        loop.create_task(publish(queue))
+        loop.create_task(consume(queue))
         loop.run_forever()
     except KeyboardInterrupt:
-        logger.info("Process interrupted")
+        logging.info("Process interrupted")
     finally:
         loop.close()
-        logger.info("Successfully shutdown the Mayhem service.")
-
-
-if __name__ == "__main__":
-    main()
+        logging.info("Successfully shutdown the Mayhem service.")
 
 
 if __name__ == "__main__":
